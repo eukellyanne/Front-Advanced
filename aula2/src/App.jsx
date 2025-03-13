d21c19226cde04cfe5ca34a61a5d17fe1672dcfb
eukellyanne <kellyannekaren23@gmail.com>
2025-03-12 21:18:10 -0300

    commit abencoado

---
+++ aula2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import RenderizandoComFuncoes from './components/RenderizandoComFuncoes'
+import RenderizaBackouFront from './components/RenderizaBackOuFront'
+
+const App = () => {
+  return (
+    <div>
+      <RenderizandoComFuncoes/>
+      <RenderizaBackouFront/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ aula2/src/components/TrabalhandoComImagens.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TrabalhandoComImagens = () => {
+  return (
+    <div>TrabalhandoComImagens</div>
+  )
+}
+
+export default TrabalhandoComImagens
